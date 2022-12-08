0eafbbf8b83a6e827e4561bb853c98ba02805c2d
Pierrotpsy <67154936+Pierrotpsy@users.noreply.github.com>
2022-12-08 21:54:24 +0100

    feat(7) import into MongoDB

--- import-data.js
+++ import-data.js
@@ -29,8 +29,30 @@ async function main() {
   console.log('Trying to connect to database')
   await mongoose.connect(process.env.MONGO_URI)
   console.log('Connection established')
+
+  await importJSON()
+  console.log('Import successful')
+  
+  console.log('Done!')
 }
 
-
+async function importJSON() {
+  for (let filmingLocation of filmingLocations) {
+    let location = new Location({
+        filmType: filmingLocation.fields.type_tournage,
+        filmProducerName: filmingLocation.fields.nom_producteur,
+        endDate: Date(filmingLocation.fields.date_fin),
+        filmName: filmingLocation.fields.nom_tournage,
+        district: filmingLocation.fields.ardt_lieu,
+        geolocation: filmingLocation.fields.geo_shape,
+        sourceLocationId: filmingLocation.fields.id_lieu,
+        filmDirectorName: filmingLocation.fields. nom_realisateur,
+        address: filmingLocation.fields.adresse_lieu,
+        startDate: Date(filmingLocation.fields.date_debut),
+        year: filmingLocation.fields.annee_tournage
+    });
+    location.save();
+  }
+}
 
 main()
